c3ec499797819a731b023f32e7dc577ad509e244
Christopher Putz <cputz@systhemis.de>
2022-12-13 15:13:42 +0100

    Gave dwarves a inspectable position

--- dwarf_plugin/src/lib.rs
+++ dwarf_plugin/src/lib.rs
@@ -1,11 +1,26 @@
 use bevy::log;
 use bevy::prelude::*;
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::RegisterInspectable;
 
 pub struct DwarfPlugin;
+
+#[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
+#[derive(Component, Debug)]
+struct Position {
+    pub x: i16,
+    pub y: i16,
+    pub elevation: i16,
+}
+
+#[derive(Component)]
+struct Dwarf;
 
 impl Plugin for DwarfPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(Self::add_dwarves);
+        #[cfg(feature = "debug")]
+        app.register_inspectable::<Position>();
         log::info!("Loaded DwarfPlugin");
     }
 }
@@ -13,8 +28,9 @@ impl Plugin for DwarfPlugin {
 impl DwarfPlugin {
     fn add_dwarves(mut commands: Commands) {
         commands.spawn(
-                Transform::from_xyz(0.0, 0.0, 0.0)
+                Dwarf
         )
-        .insert(Name::new("Dwarf"));
+        .insert(Name::new("Dwarf"))
+        .insert(Position {x: 0, y:0, elevation: 0});
     }
 }
