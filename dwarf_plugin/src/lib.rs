c64276809438d51f05ff5b60a33cad5c7b165602
tigerplush <64077786+tigerplush@users.noreply.github.com>
2022-12-13 22:36:34 +0100

    Added basic food

--- dwarf_plugin/src/lib.rs
+++ dwarf_plugin/src/lib.rs
@@ -2,7 +2,7 @@ use bevy::log;
 use bevy::prelude::*;
 #[cfg(feature = "debug")]
 use bevy_inspector_egui::RegisterInspectable;
-
+use rand::{{thread_rng, Rng}};
 pub struct DwarfPlugin;
 
 #[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
@@ -13,8 +13,18 @@ struct Position {
     pub elevation: i16,
 }
 
+impl Position {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        Position {x: rng.gen::<i16>(), y: rng.gen::<i16>(), elevation: rng.gen::<i16>() }
+    }
+}
+
 #[derive(Component)]
 struct Dwarf;
+
+#[derive(Component)]
+struct Food;
 
 #[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
 #[derive(Component, Debug)]
@@ -27,6 +37,7 @@ struct Desire {
 impl Plugin for DwarfPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(Self::add_dwarves);
+        app.add_startup_system(Self::spawn_food);
         app.add_system(Self::tick_desires);
         #[cfg(feature = "debug")]
         app.register_inspectable::<Position>();
@@ -44,6 +55,12 @@ impl DwarfPlugin {
         .insert(Name::new("Dwarf"))
         .insert(Position {x: 0, y:0, elevation: 0})
         .insert(Desire { name: Name::new("Hunger"), value: 0.0, increase: 0.1});
+    }
+
+    fn spawn_food(mut commands: Commands) {
+        commands.spawn(Food)
+        .insert(Name::new("Food"))
+        .insert(Position::random());
     }
 
     fn tick_desires(
