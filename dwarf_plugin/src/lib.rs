d23ea18819aeac22dc18a4c2b50a6a1c1031d7fa
tigerplush <64077786+tigerplush@users.noreply.github.com>
2022-12-13 22:22:40 +0100

    Added first desire

--- dwarf_plugin/src/lib.rs
+++ dwarf_plugin/src/lib.rs
@@ -16,11 +16,22 @@ struct Position {
 #[derive(Component)]
 struct Dwarf;
 
+#[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
+#[derive(Component, Debug)]
+struct Desire {
+    pub name: Name,
+    pub value: f32,
+    pub increase: f32,
+}
+
 impl Plugin for DwarfPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(Self::add_dwarves);
+        app.add_system(Self::tick_desires);
         #[cfg(feature = "debug")]
         app.register_inspectable::<Position>();
+        #[cfg(feature = "debug")]
+        app.register_inspectable::<Desire>();
         log::info!("Loaded DwarfPlugin");
     }
 }
@@ -31,6 +42,16 @@ impl DwarfPlugin {
                 Dwarf
         )
         .insert(Name::new("Dwarf"))
-        .insert(Position {x: 0, y:0, elevation: 0});
+        .insert(Position {x: 0, y:0, elevation: 0})
+        .insert(Desire { name: Name::new("Hunger"), value: 0.0, increase: 0.1});
+    }
+
+    fn tick_desires(
+        time: Res<Time>,
+        mut query:Query<&mut Desire>
+    ) {
+        for mut desire in query.iter_mut() {
+            desire.value = desire.value + desire.increase * time.delta_seconds();
+        }
     }
 }
