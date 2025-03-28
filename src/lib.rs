47513232606f710c274f12378482a25fff47b90c
tigerplush <christopher.putz@corgibase.de>
2025-03-28 11:19:26 +0100

    deleted everything, started from scratch with new project structure

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod camera;
-pub mod map;
-pub mod path;
-pub mod position;
-pub mod tools;
-
-pub use position::Position;
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,17 @@
 use bevy::prelude::*;
-
-#[cfg(feature = "debug")]
-use bevy::log::LogPlugin;
-
-use bevy_ecs_tilemap::TilemapPlugin;
-#[cfg(feature = "inspector")]
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-
-#[cfg(feature = "fps")]
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-
-mod dwarf_plugin;
-use dwarf_plugin::*;
-
-use open_fortress::*;
+use bevy::window::WindowResolution;
 
 fn main() {
     let mut app = App::new();
-
-    #[cfg(not(feature = "debug"))]
-    app.add_plugins(
-        DefaultPlugins
-            // to avoid blurry pixels
-            .set(ImagePlugin::default_nearest()),
-    );
-    #[cfg(feature = "debug")]
-    app.add_plugins(
-        DefaultPlugins
-            // to avoid blurry pixels
-            .set(ImagePlugin::default_nearest())
-            .set(LogPlugin {
-                level: bevy::log::Level::DEBUG,
-                ..default()
-            }),
-    );
-    app.add_plugins(TilemapPlugin);
-    #[cfg(feature = "inspector")]
-    app.add_plugins(WorldInspectorPlugin::new());
-    #[cfg(feature = "fps")]
-    app.add_plugins(LogDiagnosticsPlugin::default())
-        .add_plugins(FrameTimeDiagnosticsPlugin::default());
-    app.add_plugins((
-        DwarfPlugin,
-        camera::plugin,
-        map::plugin,
-        path::plugin,
-        tools::plugin,
-    ));
-    app.add_systems(Startup, spawn_food);
-    app.run();
-}
-
-#[derive(Component)]
-struct ClaimedBy(Entity);
-
-#[derive(Component)]
-struct Food;
-
-fn spawn_food(
-    mut commands: Commands,
-    mut texture_atlasses: ResMut<Assets<TextureAtlasLayout>>,
-    asset_server: Res<AssetServer>,
-) {
-    let texture_handle = asset_server.load("food.png");
-    let texture_atlas = TextureAtlasLayout::from_grid(UVec2::new(16, 16), 8, 8, None, None);
-    let texture_atlas_handle = texture_atlasses.add(texture_atlas);
-
-    commands.spawn((
-        SpriteBundle {
-            texture: texture_handle.clone(),
-            transform: Transform::from_translation(Position::new(5, 5, 0).into_world()),
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: format!("Open Fortress {}", env!("CARGO_PKG_VERSION")),
+            resolution: WindowResolution::new(1280.0, 720.0),
+            resizable: false,
             ..default()
-        },
-        TextureAtlas {
-            layout: texture_atlas_handle.clone(),
-            index: 34,
-        },
-        Food,
-        Name::from("Food"),
-    ));
+        }),
+        ..default()
+    }));
+    app.add_plugins((common::plugin, splashscreen::plugin));
+    app.run();
 }
