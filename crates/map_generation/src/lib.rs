76a07725e21c6430a2783105297ddaab2d9a2660
tigerplush <christopher.putz@corgibase.de>
2025-03-31 13:21:39 +0200

    simplified generating chunks, added deleting chunks out of range

--- crates/map_generation/src/lib.rs
+++ crates/map_generation/src/lib.rs
@@ -1,10 +1,12 @@
+use std::ops::{Range, RangeInclusive};
+
 use assets::tileset_asset::{TILE_SIZE, TileType, TilesetAsset};
 use bevy::{platform_support::collections::HashMap, prelude::*};
 use common::{states::AppState, traits::AsVec2};
 
 pub fn plugin(app: &mut App) {
     app.add_systems(OnEnter(AppState::MainGame), spawn_world)
-        .add_systems(Update, request_chunks.run_if(in_state(AppState::MainGame)))
+        .add_systems(Update, (request_chunks, delete_chunks).run_if(in_state(AppState::MainGame)))
         .add_observer(on_add_chunk_visualisation);
 }
 
@@ -69,7 +71,7 @@ struct ChunkVisualisation(IVec3);
 impl ChunkVisualisation {
     fn new(coordinates: IVec3) -> impl Bundle {
         (
-            Name::new(format!("{}", coordinates)),
+            Name::new(format!("Chunk {}", coordinates)),
             ChunkVisualisation(coordinates),
             Transform::from_xyz(
                 coordinates.x as f32 * CHUNK_SIZE.x as f32 * TILE_SIZE.x,
@@ -87,39 +89,24 @@ fn request_chunks(
     chunks: Query<&ChunkVisualisation>,
     mut commands: Commands,
 ) {
-    // check which chunks should be visible from the camera
-    let (transform, projection) = camera_transform.into_inner();
-    let Projection::Orthographic(values) = projection else {
+    let Some((x_range, y_range, z_range)) =
+        calculate_visible_chunk_ranges_from_single(camera_transform)
+    else {
         return;
     };
-    let camera_x = transform.translation.x;
-    let camera_y = transform.translation.y;
 
-    let chunk_size_x = CHUNK_SIZE.x as f32 * TILE_SIZE.x;
-    let chunk_size_y = CHUNK_SIZE.y as f32 * TILE_SIZE.y;
-    let min_x = camera_x + values.area.min.x;
-    let max_x = camera_x + values.area.max.x;
-    let min_y = camera_y + values.area.min.y;
-    let max_y = camera_y + values.area.max.y;
-
-    let min_chunk_x = (min_x / chunk_size_x).floor() as i32;
-    let max_chunk_x = (max_x / chunk_size_x).ceil() as i32;
-    let min_chunk_y = (min_y / chunk_size_y).floor() as i32;
-    let max_chunk_y = (max_y / chunk_size_y).ceil() as i32;
-
-    // are the chunks already there?
     let mut requested_chunks = vec![];
-    for x in min_chunk_x..max_chunk_x {
-        for y in min_chunk_y..max_chunk_y {
-            for z in -1..=1 {
+    for x in x_range {
+        for y in y_range.clone() {
+            for z in z_range.clone() {
                 requested_chunks.push(IVec3::new(x, y, z));
             }
         }
     }
-    // requested_chunks.push(IVec3::NEG_Z);
-    // if not, spawn them
     for coordinates in requested_chunks {
+        // are the chunks already there?
         if let None = chunks.iter().find(|chunk| chunk.0 == coordinates) {
+            // if not, spawn them
             info!("spawning chunk at {:?}", coordinates);
             let a = world_map
                 .chunks
@@ -128,6 +115,60 @@ fn request_chunks(
             commands.spawn(ChunkVisualisation::new(coordinates));
         }
     }
+}
+
+fn delete_chunks(
+    camera_transform: Single<(&Transform, &Projection)>,
+    chunks: Query<(Entity, &ChunkVisualisation)>,
+    mut commands: Commands,
+) {
+    let Some((x_range, y_range, z_range)) =
+        calculate_visible_chunk_ranges_from_single(camera_transform)
+    else {
+        return;
+    };
+    for (entity, chunk) in &chunks {
+        let coordinates = chunk.0;
+        if !x_range.contains(&coordinates.x)
+            || !y_range.contains(&coordinates.y)
+            || !z_range.contains(&coordinates.z)
+        {
+            info!("despawing chunk at {:?}", coordinates);
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn calculate_visible_chunk_ranges_from_single(
+    camera_transform: Single<(&Transform, &Projection)>,
+) -> Option<(Range<i32>, Range<i32>, RangeInclusive<i32>)> {
+    let (transform, projection) = camera_transform.into_inner();
+    let Projection::Orthographic(values) = projection else {
+        return None;
+    };
+    Some(calculate_visible_chunk_ranges(transform, values))
+}
+
+/// Calculates which chunks are currently visible
+fn calculate_visible_chunk_ranges(
+    transform: &Transform,
+    projection: &OrthographicProjection,
+) -> (Range<i32>, Range<i32>, RangeInclusive<i32>) {
+    let camera_x = transform.translation.x;
+    let camera_y = transform.translation.y;
+
+    let chunk_size_x = CHUNK_SIZE.x as f32 * TILE_SIZE.x;
+    let chunk_size_y = CHUNK_SIZE.y as f32 * TILE_SIZE.y;
+    let min_x = camera_x + projection.area.min.x;
+    let max_x = camera_x + projection.area.max.x;
+    let min_y = camera_y + projection.area.min.y;
+    let max_y = camera_y + projection.area.max.y;
+
+    let min_chunk_x = (min_x / chunk_size_x).floor() as i32;
+    let max_chunk_x = (max_x / chunk_size_x).ceil() as i32;
+    let min_chunk_y = (min_y / chunk_size_y).floor() as i32;
+    let max_chunk_y = (max_y / chunk_size_y).ceil() as i32;
+    (min_chunk_x..max_chunk_x, min_chunk_y..max_chunk_y, -1..=1)
 }
 
 const CHUNK_SIZE: UVec3 = UVec3::new(16, 16, 16);
