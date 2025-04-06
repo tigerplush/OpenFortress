72ac0149244d483f661800393a3e0c7a661ff807
tigerplush <christopher.putz@corgibase.de>
2025-04-06 13:09:09 +0200

    moved neighbor trait to common crate

--- crates/common/src/traits.rs
+++ crates/common/src/traits.rs
@@ -30,3 +30,26 @@ impl AsVec2 for (u32, u32) {
         Vec2::new(self.0 as f32, self.1 as f32)
     }
 }
+
+pub trait Neighbors<T> {
+    /// Returns all neighbors with their squared cost.
+    /// 
+    /// The order is:
+    /// ```
+    /// NW, N, NE,
+    ///  W,     E,
+    /// SW, S, SE
+    /// ```
+    fn neighbors(&self) -> Vec<(T, u32)>;
+}
+
+#[rustfmt::skip]
+impl Neighbors<IVec3> for IVec3 {
+    fn neighbors(&self) -> Vec<(IVec3, u32)> {
+        vec![
+            (self + IVec3::new(-1,  1,  0), 2), (self + IVec3::new( 0,  1,  0), 1), (self + IVec3::new( 1,  1,  0), 2),
+            (self + IVec3::new(-1,  0,  0), 1),                                     (self + IVec3::new( 1,  0,  0), 1),
+            (self + IVec3::new(-1, -1,  0), 2), (self + IVec3::new( 0, -1,  0), 1), (self + IVec3::new( 1, -1,  0), 2),
+        ]
+    }
+}
