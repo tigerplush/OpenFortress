cf3be15bdfca98907cadd750226f4c538be74472
Thomas S. Mjåland <megabjarne@hotmail.com>
2025-06-16 00:14:12 +0200

    renamed BlockCoordinates to ChunkBlockCoordinates

--- crates/common/src/types.rs
+++ crates/common/src/types.rs
@@ -34,10 +34,10 @@ impl Neighbors<WorldCoordinates> for WorldCoordinates {
 #[derive(Clone, Copy, PartialEq, Reflect)]
 pub struct ChunkCoordinates(pub IVec3);
 /// Coordinates of a block within a chunk
-pub struct BlockCoordinates(pub UVec3);
+pub struct ChunkBlockCoordinates(pub UVec3);
 
-impl From<(u32, u32, u32)> for BlockCoordinates {
+impl From<(u32, u32, u32)> for ChunkBlockCoordinates {
     fn from(value: (u32, u32, u32)) -> Self {
-        BlockCoordinates(UVec3::new(value.0, value.1, value.2))
+        ChunkBlockCoordinates(UVec3::new(value.0, value.1, value.2))
     }
 }
